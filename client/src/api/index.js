de24b9a144c7ccdb70e707f151dff6d4b83bb59a
Joel <148845212+joelpaul18@users.noreply.github.com>
2026-08-31 12:51:05 +0530

    Update API base URL to new server address

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://54.226.126.255:5000/api',
+    baseURL: 'http://3.85.14.16:5000/api',
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
